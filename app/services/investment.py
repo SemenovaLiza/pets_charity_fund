6aca1e998317cc0e1ebb0a2e85eb863ec0a5ec1b
Семенова Елизавета <thealeeeaaa23@gmail.com>
2024-01-26 16:10:11 +0300

    changed donation system

--- app/services/investment.py
+++ app/services/investment.py
@@ -3,49 +3,18 @@ from typing import List, Union
 
 from app.models import CharityProject, Donation
 
+from datetime import datetime
+from typing import List
 
-async def close_donation_for_obj(obj_in: Union[CharityProject, Donation]):
-    """
-    Закрывает проект, когда внесенные пожертвования
-    покрывают требуемую сумму.
-    """
-    obj_in.invested_amount = obj_in.full_amount
-    obj_in.fully_invested = True
-    obj_in.close_date = datetime.now()
-    return obj_in
-
-
-async def money_distribution(
-    obj_in: Union[CharityProject, Donation],
-    obj_model: Union[CharityProject, Donation],
-) -> Union[CharityProject, Donation]:
-    """
-    Функция отвечает за распределение новых пожертвований
-    между проектами, не собравшими требуемую для закрытия сумму.
-    """
-    free_amount_in = obj_in.full_amount - obj_in.invested_amount
-    free_amount_in_model = obj_model.full_amount - obj_model.invested_amount
-
-    if free_amount_in > free_amount_in_model:
-        obj_in.invested_amount += free_amount_in_model
-        await close_donation_for_obj(obj_model)
-
-    elif free_amount_in == free_amount_in_model:
-        await close_donation_for_obj(obj_in)
-        await close_donation_for_obj(obj_model)
-
-    else:
-        obj_model.invested_amount += free_amount_in
-        await close_donation_for_obj(obj_in)
-
-    return obj_in, obj_model
-
+from app.models.charity_project import CharityProject
 
 def investing_process(
     target: CharityProject,
     sources: List[CharityProject]
 ) -> List[CharityProject]:
     modified = []
+    if not target.invested_amount and target.invested_amount != 0:
+        target.invested_amount = 0
     for source in sources:
         to_invest = target.full_amount - target.invested_amount
         for obj in (target, source):
